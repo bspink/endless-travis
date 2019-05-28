6c1585c6c43b4b480b227dfbb0ab0747a1c52b8b
Bradley Spink <bradley.spink@gmail.com>
2019-05-29 05:39:39 +1000

    hmm

--- make-commit.sh
+++ make-commit.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote set-url origin https://bspink:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 npm version minor
+echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 git push origin ${TRAVIS_BRANCH}
