3e43184d07a6c87d0a36d0643c5f86fd715a49c8
Bradley Spink <bradley.spink@gmail.com>
2019-05-29 05:15:59 +1000

    let's test this bad boy out

--- make-commit.sh
+++ make-commit.sh
@@ -1,5 +1,5 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote add origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git remote set-url origin https://bspink:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 npm version minor
 git push origin ${TRAVIS_BRANCH}
